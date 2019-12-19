727d9154266eb3a400e0bb019429376a355b8188
(Noah) Hanlun Song <hsong@air.org>
2019-12-19 13:54:31 -0500

    Remove SSH banner

--- header_footer_src/index.js
+++ header_footer_src/index.js
@@ -1,30 +1,30 @@
-import Vue from "vue"
-import Header from "./components/header.vue"
-import Footer from "./components/footer.vue"
+import Vue from "vue";
+import Header from "./components/header.vue";
+import Footer from "./components/footer.vue";
 
 // temporary banner
-import SSHNotif from './components/SSHNotif.vue'
+// import SSHNotif from './components/SSHNotif.vue'
 
-const header = document.createElement("div")
-document.body.prepend(header)
-
-new Vue({
-    el: header,
-    render: h => h(Header)
-})
-
-const footer = document.createElement("div")
-document.body.append(footer)
+const header = document.createElement("div");
+document.body.prepend(header);
 
 new Vue({
-    el: footer,
-    render: h => h(Footer)
-})
+  el: header,
+  render: h => h(Header)
+});
 
-const sshNotif = document.createElement("div")
-document.body.append(sshNotif)
+const footer = document.createElement("div");
+document.body.append(footer);
 
 new Vue({
-    el: sshNotif,
-    render: h => h(SSHNotif)
-})
+  el: footer,
+  render: h => h(Footer)
+});
+
+// const sshNotif = document.createElement("div")
+// document.body.append(sshNotif)
+
+// new Vue({
+//     el: sshNotif,
+//     render: h => h(SSHNotif)
+// })
